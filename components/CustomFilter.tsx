68410c77ad266b6f324c0a1f73903972e2c03b78
mobitechdesenvolvimento <joaovitorcosta-bjj@hotmail.com>
2024-09-29 13:01:23 -0300

    car_showcase

---
+++ components/CustomFilter.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CustomFIlter = () => {
+  return <div>CustomFilter</div>;
+};
+
+export default CustomFIlter;
---
+++ utils/index.ts
@@ -0,0 +1,37 @@
+import { CarProps } from "@/types";
+
+export async function fetchCars() {
+  // Set the required headers for the API request
+  const headers = {
+    "x-rapidapi-key": "66c409f5c3msh4bc56c41e6b620ap1db9ecjsn49f3461ae8e9",
+    "x-rapidapi-host": "cars-by-api-ninjas.p.rapidapi.com",
+  };
+
+  // Set the required headers for the API request
+  const response = await fetch(
+    `https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=q3`,
+    {
+      headers: headers,
+    }
+  );
+
+  // Parse the response as JSON
+  const result = await response.json();
+
+  return result;
+}
+
+export const calculateCarRent = (city_mpg: number, year: number) => {
+  const basePricePerDay = 50; // Base rental price per day in dollars
+  const mileageFactor = 0.1; // Additional rate per mile driven
+  const ageFactor = 0.05; // Additional rate per year of vehicle age
+
+  // Calculate additional rate based on mileage and age
+  const mileageRate = city_mpg * mileageFactor;
+  const ageRate = (new Date().getFullYear() - year) * ageFactor;
+
+  // Calculate total rental rate per day
+  const rentalRatePerDay = basePricePerDay + mileageRate + ageRate;
+
+  return rentalRatePerDay.toFixed(0);
+};
